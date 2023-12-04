22e1d97095c6937cd691793243424780b914f2af
Leon Pawelzik <leon.pawelzik@gmail.com>
2023-12-04 16:13:00 +0100

    Added API functionality. Uploading Image, returning Image and Variable(Classification)

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
+from starlette.responses import StreamingResponse
+import io
 
 app = FastAPI()
 
@@ -12,6 +14,24 @@ def read_root():
         dict: A dictionary with the greeting message.
     """
     return {"Hello": "World"}
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    """
+    Uploads an image file and returns a streaming response with the file contents.
+
+    Parameters:
+        file (UploadFile): The image file to be uploaded.
+
+    Returns:
+        StreamingResponse: A streaming response containing the file contents.
+
+    Raises:
+        None
+    """
+    contents = await file.read()  # Read file contents
+    headers = {"Classification": "This image is blurry"}
+    return StreamingResponse(io.BytesIO(contents), media_type=file.content_type, headers=headers)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
